8fff27c2c438525125801666e453da81a01f42ce
jrodowick <jacobrodowick@gmail.com>
2019-03-25 10:45:31 -0700

    Changes API call to select from menu click/Adds function to animate menu back into view

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var app = new Vue({
   el: '#app',
   data: {
     message: "Star Wars Encyclopedia",
-    response: {}
+    response: {},
+    seen: false
   },
   methods: {
-    getData: function() {
-      fetch('https://swapi.co/api/people/')
+    getData: function(choice) {
+      fetch('https://swapi.co/api/' + choice)
       .then(data => data.json())
       .then(results => {
-        console.log(results.results)
-        app.response = results.results
+        console.log(results)
+        app.response = results
       })
     },
     hideMenu: function(event) {
@@ -19,11 +20,24 @@ var app = new Vue({
         $(this).delay(100 * index).animate({
           opacity: 0.0,
           paddingLeft: '+=200'
-        })
-      })
+        });
+      });
+      this.getData(event.target.textContent.toLowerCase())
+      this.showHome();
+    },
+    showHome: function() {
+      app.seen = true;
+    },
+    hideHome: function() {
+      app.seen = false;
+      let $li = $('li')
+      $li.each(function(index) {
+        $(this).delay(100 * index).animate({
+          opacity: 1.0,
+          paddingLeft: '-=200'
+        });
+      });
+
     }
   },
-  beforeMount() {
-    this.getData();
-  }
 })
